9b5c62f349759d0913563022917acb1bff6a6aa1
Odarka Kruk <odarochka1802@gmail.com>
2022-08-30 20:58:13 +0300

    Pov

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+int result_2 = Convert.ToInt32(Math.Pow(number,2));
+
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
